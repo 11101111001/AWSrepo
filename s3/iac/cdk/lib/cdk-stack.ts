626933adbff806c23b8b9630e8f1f88d64b58cf3
Carter Weidman <carter.weidman@outlook.com>
2025-07-27 21:06:58 -0400

    "simple cdk example"

---
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -0,0 +1,17 @@
+import {RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'MYBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: true,
+      removalPolicy: RemovalPolicy.RETAIN
+    });
+  }
+}
